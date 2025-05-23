98ba48fe19d092763cbe0459b0d4719a836cce32
Youssef Quaisse <91441379+IHatePineapples@users.noreply.github.com>
2025-05-23 17:52:22 +0100

    Add dependencies in nix file.

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    python312Packages.torchWithCuda
+    gcc
+    cmake
+    protobuf
+    abseil-cpp
+  ];
+}
